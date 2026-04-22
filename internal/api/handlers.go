2933c6aafc936303f8aecab16413ad409c601632
mikeder <mikedernet@gmail.com>
2026-04-22 13:28:44 -0400

    sse

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
+	"log/slog"
 	"net/http"
 
 	"netdash/internal/alerts"
@@ -9,10 +11,45 @@ import (
 )
 
 func RegisterRoutes(mux *http.ServeMux, store *device.Store, alerts *alerts.Manager) {
-
 	mux.Handle("/", http.FileServer(http.Dir("./static")))
 
 	mux.HandleFunc("/devices", func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(store.All())
+	})
+
+	mux.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		flusher, ok := w.(http.Flusher)
+		if !ok {
+			http.Error(w, "streaming not supported", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+
+		send := func() {
+			data, err := json.Marshal(store.All())
+			if err != nil {
+				slog.Warn("sse: failed to marshal devices", "err", err)
+				return
+			}
+			fmt.Fprintf(w, "data: %s\n\n", data)
+			flusher.Flush()
+		}
+
+		ch := store.Subscribe()
+		defer store.Unsubscribe(ch)
+
+		send()
+
+		for {
+			select {
+			case <-r.Context().Done():
+				return
+			case <-ch:
+				send()
+			}
+		}
 	})
 }
--- internal/device/store.go
+++ internal/device/store.go
@@ -8,10 +8,11 @@ import (
 )
 
 type Store struct {
-	mu     sync.Mutex
-	data   map[string]*Device
-	alerts chan string
-	db     *sql.DB
+	mu          sync.Mutex
+	data        map[string]*Device
+	alerts      chan string
+	db          *sql.DB
+	subscribers []chan struct{}
 }
 
 func NewStore() *Store {
@@ -26,9 +27,27 @@ func (s *Store) SetDB(db *sql.DB) {
 	s.db = db
 }
 
-func (s *Store) Update(ip string, update func(d *Device)) {
+func (s *Store) Subscribe() chan struct{} {
+	ch := make(chan struct{}, 1)
+	s.mu.Lock()
+	s.subscribers = append(s.subscribers, ch)
+	s.mu.Unlock()
+	return ch
+}
+
+func (s *Store) Unsubscribe(ch chan struct{}) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
+	for i, sub := range s.subscribers {
+		if sub == ch {
+			s.subscribers = append(s.subscribers[:i], s.subscribers[i+1:]...)
+			return
+		}
+	}
+}
+
+func (s *Store) Update(ip string, update func(d *Device)) {
+	s.mu.Lock()
 
 	d, exists := s.data[ip]
 	if !exists {
@@ -48,6 +67,18 @@ func (s *Store) Update(ip string, update func(d *Device)) {
 			slog.Warn("store: failed to record uptime change", "ip", ip, "err", err)
 		}
 	}
+
+	subs := make([]chan struct{}, len(s.subscribers))
+	copy(subs, s.subscribers)
+
+	s.mu.Unlock()
+
+	for _, ch := range subs {
+		select {
+		case ch <- struct{}{}:
+		default:
+		}
+	}
 }
 
 func boolToInt(b bool) int {
@@ -56,5 +87,12 @@ func boolToInt(b bool) int {
 }
 
 func (s *Store) All() map[string]*Device {
-	return s.data
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	out := make(map[string]*Device, len(s.data))
+	for k, v := range s.data {
+		d := *v
+		out[k] = &d
+	}
+	return out
 }
